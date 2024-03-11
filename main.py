09fe76865f77d6376fa4ecc4b3b866d718d066e4
ishay1997 <ishaylevy8@gmail.com>
2024-03-11 13:16:47 +0200

    delete the selenium part that change photos by the title of the post

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-import requests
+import wait as wait
 from flask import Flask, render_template
+import requests
+from selenium import webdriver
+from selenium.common import TimeoutException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-posts = requests.get("https://api.npoint.io/cbaf2e1513ffda165f3a").json()
+# Assuming 'driver' is your WebDriver instance
+# Wait up to 10 seconds for the element to be present
+
+posts = requests.get("https://api.npoint.io/f4dc99f755ee29815828").json()
 app = Flask(__name__)
-
-
-@app.route('/')
-def get_all_posts():
-    return render_template("index.html", posts =posts)
+image_status = {}
 
 @app.route('/about')
 def about():
@@ -17,12 +23,18 @@ def about():
 def contact():
     return render_template("contact.html")
 
+
+
+@app.route('/')
+def get_all_posts():
+    global image_status
+    return render_template("index.html", posts=posts)
+
+# Your other routes here
+
 @app.route('/show_post/<int:index>')
 def show_post(index):
-    requested_post = None
-    for post in posts:
-        if post["id"] == index:
-            requested_post = post
+    requested_post = next((post for post in posts if post["id"] == index), None)
     return render_template("post.html", post=requested_post)
 
 if __name__ == "__main__":
